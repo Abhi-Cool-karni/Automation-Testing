b6e022a7551c01e6dc48b6343bd10acb0c471364
abhi-cool-karni <abhishekkul2000@gmail.com>
2023-07-25 23:24:45 +0530

    Ninth commit

--- PythonSelFramework/tests/conftest.py
+++ PythonSelFramework/tests/conftest.py
@@ -5,9 +5,9 @@ import pytest
 
 @pytest.fixture(scope="class")
 def setup(request):
-    
+
     service_obj = Service("C:/Users/Abhishek Kulkarni/Desktop/AK/Software Testing/Automation/chromedriver_win32/chromedriver")
-    driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+    driver = webdriver.Chrome(service=service_obj)
     driver.implicitly_wait(5)
     driver.maximize_window()
     driver.get("https://rahulshettyacademy.com/angularpractice/")
