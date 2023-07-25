31c037722d40fc71ca622b00a4e11bc10df4de52
abhi-cool-karni <abhishekkul2000@gmail.com>
2023-07-25 21:52:06 +0530

    Eighth commit

---
+++ PythonSelFramework/tests/conftest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import pytest
+
+@pytest.fixture(scope="class")
+def setup(request):
+    
+    service_obj = Service("C:/Users/Abhishek Kulkarni/Desktop/AK/Software Testing/Automation/chromedriver_win32/chromedriver")
+    driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+    driver.implicitly_wait(5)
+    driver.maximize_window()
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    request.cls.driver = driver
+    yield
+    driver.close()
---
+++ PythonSelFramework/tests/test_e2e.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+from utilities.baseClass import BaseClass
+
+class TestOne(BaseClass):
+    def test_e2e(self):
+        # Regular expression - instead of targeting value full give piece of information.
+        # CSS - a[href*='shop']  Xpath - //a[contains(@href,'shop')]
+
+        self.driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+        cards = self.driver.find_elements(By.XPATH, "//div[@class='card h-100']")
+
+        # selecting and checking out the Blackberry product
+        for card in cards:
+            cardName = card.find_element(By.XPATH, "div/h4/a").text
+            if cardName == "Blackberry":
+                card.find_element(By.XPATH, "div/button").click()
+
+        # Moving to checkout page
+        self.driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+
+        # Making checkout of product
+        self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+
+        # Selecting location for checkout
+        self.driver.find_element(By.ID, "country").send_keys("Ind")
+
+        # Waiting till location is load and displayed
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(expected_conditions.presence_of_element_located((
+            By.LINK_TEXT, "India")))
+        self.driver.find_element(By.LINK_TEXT, "India").click()
+
+        # Clicking an checkbox
+        self.driver.find_element(
+            By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
+
+        # Submitting location
+        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        # Success validation
+        successText = self.driver.find_element(By.CLASS_NAME, "alert-success").text
+
+        assert "Success!" in successText
