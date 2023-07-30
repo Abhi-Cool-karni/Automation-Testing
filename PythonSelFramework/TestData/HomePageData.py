f88f6305ca6b144bf361e2789697986496c8a570
abhi-cool-karni <abhishekkul2000@gmail.com>
2023-07-30 18:44:09 +0530

    18 commit

---
+++ PythonSelFramework/TestData/HomePageData.py
@@ -0,0 +1,4 @@
+class HomepageData:
+
+    test_Homepage_Data = [{"Name": "Abhishek", "Email": "abhishekkul000@gmail.com", "Password": "abhishek@1234", "Gender": "Male"},
+        {"Name": "Priya", "Email": "priya@gmail.com", "Password": "priya@1234", "Gender": "Female"}]
--- PythonSelFramework/tests/test_anugularPractice.py
+++ PythonSelFramework/tests/test_anugularPractice.py
@@ -1,22 +1,28 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+import pytest
 from PageObjectMechanism.HomePage import HomePage
+from TestData.HomePageData import HomepageData
 from Utility.BaseClass import baseClass
 
 
 class TestHomePage(baseClass):
 
-    def test_FormSubmission(self):
-
+    def test_FormSubmission(self, getData):
         homepage = HomePage(self.driver)
-        homepage.getName().send_keys('Abhishek') #NAME
-        homepage.getEmail().send_keys("abhishekkul2000@gmail.com")  #EMAIL
-        homepage.getPassword().send_keys("abhishek@1234")   #PASSWORD
-        homepage.getCheckbox().click()  #CHECKBOX
-        self.selectOptioByText(homepage.selectDropdown(), "Female") #GENDER SELECTION
-        homepage.ClickRadio().click()   #Employment Status
-        homepage.clickSubmit().click()  #Submit button
+        homepage.getName().send_keys(getData["Name"])  # NAME
+        homepage.getEmail().send_keys(getData["Email"])  # EMAIL
+        homepage.getPassword().send_keys(getData["Password"])  # PASSWORD
+        homepage.getCheckbox().click()  # CHECKBOX
+        self.selectOptioByText(homepage.selectDropdown(), getData["Gender"])  # GENDER SELECTION
+        homepage.ClickRadio().click()  # Employment Status
+        homepage.clickSubmit().click()  # Submit button
         message = homepage.getSuccess()
         print(message)
         assert "Success" in message
+        self.driver.refresh()
+
+    # Implementing Data driven mechanism by removing hard coding data from tests
+    # @pytest.fixture(params=[("Abhishek", "abhishekkul000@gmail.com", "abhishek@1234", "Male"), ("Priya", "priya@gmail.com", "priya@1234", "Female")])
+    # Params supports both tuple and dictionary
+    @pytest.fixture(params=HomepageData.test_Homepage_Data)
+    def getData(self, request):
+        return request.param
